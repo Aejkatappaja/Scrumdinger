07ddb11be83c16e2e2688ca7e030620b882d3b07
Aejkatappaja <joverjlj@gmail.com>
2023-07-18 12:41:52 +0200

    Initial Commit

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Frank Jover on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
