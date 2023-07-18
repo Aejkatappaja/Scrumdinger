74e2f093017ef50a5ceb6a921fcf2f15d1587287
Aejkatappaja <joverjlj@gmail.com>
2023-07-18 19:43:14 +0200

    FInished View

--- ScrumDinger/MeetingView.swift
+++ ScrumDinger/MeetingView.swift
@@ -13,16 +13,29 @@ struct MeetingView: View {
             ProgressView(value: 5, total: 15)
             HStack {
                 VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
+                    Text("Seconds Elapsed").font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
                 Spacer()
                 VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
+                    Text("Seconds Remaining").font(.caption)
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            .accessibilityElement(children: .ignore)
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next Speaker")
+            }
         }
+        .padding()
     }
 }
 
