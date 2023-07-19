90079217c431bd1b3f2481a38e82ffed5df4fd40
Aejkatappaja <joverjlj@gmail.com>
2023-07-19 21:35:02 +0200

    understanding .accessibilityLabel

--- ScrumDinger/Views/CardView.swift
+++ ScrumDinger/Views/CardView.swift
@@ -12,11 +12,14 @@ struct CardView: View {
     var body: some View {
         VStack(alignment: .leading) { Text(scrum.title)
             .font(.headline)
+            .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack { Label("\(scrum.attendees.count)", systemImage: "person.3")
+                .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minutes meeting")
             }
             .font(.caption)
         }
