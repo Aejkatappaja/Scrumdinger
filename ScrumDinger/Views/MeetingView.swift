c8e8fd23f51353f673fd4e07ba9a22fcae20c335
Aejkatappaja <joverjlj@gmail.com>
2023-08-05 12:51:31 +0200

    Extract the meeting footer

--- ScrumDinger/Views/MeetingView.swift
+++ ScrumDinger/Views/MeetingView.swift
@@ -14,21 +14,13 @@ struct MeetingView: View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
-
             VStack {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
                                   secondsRemaining: scrumTimer.secondsRemaining,
                                   theme: scrum.theme)
                 Circle()
                     .strokeBorder(lineWidth: 24)
-                HStack {
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "forward.fill")
-                    }
-                    .accessibilityLabel("Next speaker")
-                }
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
         .padding()
@@ -37,7 +29,7 @@ struct MeetingView: View {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
             scrumTimer.startScrum()
         }
-        onDisappear {
+        .onDisappear {
             scrumTimer.stopScrum()
         }
         .navigationBarTitleDisplayMode(.inline)
