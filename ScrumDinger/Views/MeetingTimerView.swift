92ec1312bc6f8eb17bda4742f058dac04b9dede7
Aejkatappaja <joverjlj@gmail.com>
2023-08-23 23:02:18 +0200

    init TimerView

---
+++ ScrumDinger/Views/MeetingTimerView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingTimerView.swift
+//  ScrumDinger
+//
+//  Created by Frank Jover on 23/08/2023.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingTimerView()
+    }
+}
